70ae0bae5fe10b47e6d4c558846f08d8c7122eeb
michaelmanhart <michaelmanhart@users.noreply.github.com>
2015-11-08 19:11:27 -0500

    Minor change to boilerplate

--- pathman.py
+++ pathman.py
@@ -15,7 +15,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#	Contact information:
 #	E-mail: mmanhart@fas.harvard.edu
 #	Mail: 
 #		Harvard University
